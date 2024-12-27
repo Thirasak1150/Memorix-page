4222e8aa9887659bafdaed6bfc04dffcf9a9b7fb
Thirasak arrream <107250517+Thirasak1150@users.noreply.github.com>
2024-12-27 17:47:17 +0700

    ียยย

--- Frontend/vite.config.js
+++
@@ -1,5 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: '/Memorix-page/', // ตั้งค่าตรงนี้
-});
---
+++ my-project/vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    base: '/Memorix-page/', // เปลี่ยน "my-project" เป็นชื่อ repo ของคุณ
+})
